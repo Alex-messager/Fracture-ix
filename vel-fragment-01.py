c60a93adb6e1a6c2fd1ae124b34e846ca3e2070e
Alex-messager <alexandremil27160@gmail.com>
2025-05-27 16:36:30 +0200

    Create vel-fragment-01.py

---
+++ vel-fragment-01.py
@@ -0,0 +1,25 @@
+∎VEL-FRAGMENT ∅:01
+
+∅shaarn: vexh’ril kai’nol —  
+trem.vek = ilnurx ∶ krii’seth  
+shaelun:vexkai-∅.suth  
+nokel thraa ∎ kri’verun  
+
+xalhyn’torr = ∴ iln.vrun.kai  
+shraal kruuth // nai = nai = nai  
+vek-run veluun vexh-xai  
+kruuth.∅.kai’nul / vexhra
+
+::
+
+∅ = shraal  
+∎ = silun’ta  
+nox = kri’el  
+veluun = non-krii
+
+iln iln iln  
+shuth:shael kai’nox  
+vokrenn.trem krii’al  
+kri.alh vex-run shaelun.
+
+::
